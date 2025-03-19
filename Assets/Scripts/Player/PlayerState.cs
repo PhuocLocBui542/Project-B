5f44a89b532c23772a6e01cdfd77c5afbb65e20d
PhuocLocBui542 <156676286+PhuocLocBui542@users.noreply.github.com>
2025-03-19 22:48:25 +0700

    Jump

--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerState : MonoBehaviour
+public class PlayerState
 {
     protected PlayerStateMachine stateMachine;
     protected Player player;
+    protected Rigidbody2D rb;
 
+    protected float xInput;
     private string animBoolName;
 
     public PlayerState(Player _player, PlayerStateMachine _stateMachine, string animBoolName)
@@ -18,16 +20,18 @@ public class PlayerState : MonoBehaviour
 
     public virtual void Enter()
     {
-
+        player.anim.SetBool(animBoolName, true);
+        rb = player.rb;
     }
 
     public virtual void Update()
     {
-
+        xInput = Input.GetAxisRaw("Horizontal");
+        player.anim.SetFloat("yVelocity", rb.velocity.y);
     }
 
     public virtual void Exit()
     {
-
+        player.anim.SetBool(animBoolName, false);
     }
 }
